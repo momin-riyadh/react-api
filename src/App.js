86f397e365732dccf4db50811a6d8d61b172ede5
mominriyadh <riyadh@codesign.com.bd>
2020-09-20 22:14:32 +0600

    fix and deploy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Headlines from './components/NewsHeadlines/Headlines'
 export default function App() {
     return (
         <div className="container-fluid my-5">
+            <h3 className="text-center mb-5">
+                React Working with Fetch API
+            </h3>
             <Headlines/>
         </div>
     );
